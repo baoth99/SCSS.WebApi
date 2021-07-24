0ed9a989059e5c2aa1fcc14cc448911dc699ce4e
BaoTH <62712557+Baoth99@users.noreply.github.com>
2021-07-24 17:53:56 +0700

    remove redurant ittem

--- SCSS.WebApi/Controllers/AdminControllers/BaseAdminController.cs
+++ SCSS.WebApi/Controllers/AdminControllers/BaseAdminController.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SCSS.Utilities.Constants;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SCSS.WebApi.Controllers.AdminControllers
 {
